3303c6f6e738134a16aa9fa3ee846af224cfba43
Alex <alex.kliger@gmail.com>
2025-01-26 17:33:39 -0500

    fix: remove mouse effect temporarily

--- src/components/ParticleEffect/_index.tsx
+++ src/components/ParticleEffect/_index.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useRef } from "react";
 import {
-  Particle,
   createParticles,
-  createMouseParticle,
   animateParticles,
-  animateMouseParticle,
 } from "./particle";
 
 interface ParticleEffectProps {
@@ -13,7 +10,6 @@ interface ParticleEffectProps {
 
 const ParticleEffect = ({ particleDensity = 0.0001 }: ParticleEffectProps) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const mousePos = useRef({x: 0, y: 0});
 
     useEffect(() => {
       const canvas = canvasRef.current;
@@ -26,7 +22,6 @@ const ParticleEffect = ({ particleDensity = 0.0001 }: ParticleEffectProps) => {
       canvas.height = window.innerHeight;
 
       let particleArr;
-      let mouseParticle;
       
       // Resize canvas
       const handleResize = () => {
@@ -35,27 +30,16 @@ const ParticleEffect = ({ particleDensity = 0.0001 }: ParticleEffectProps) => {
         // DRY
         particleArr = createParticles(canvas, particleDensity);
         animateParticles(ctx, particleArr);
-        mouseParticle = new Particle(mousePos.current.x, mousePos.current.y, 0, 0, 5);
-        animateMouseParticle(ctx, mouseParticle, mousePos.current)
       }
       window.addEventListener('resize', handleResize);
-
-      const handleMouseMove = (event: MouseEvent) => {
-        mousePos.current.x = event.clientX
-        mousePos.current.y = event.clientY
-      }
-      window.addEventListener('mousemove', handleMouseMove)
 
       // Create particles and begin animation
       // DRY
       particleArr = createParticles(canvas, particleDensity);
       animateParticles(ctx, particleArr);
-      mouseParticle = createMouseParticle(mousePos.current.x, mousePos.current.y);
-      animateMouseParticle(ctx, mouseParticle, mousePos.current);
 
       return () => {
         window.removeEventListener('resize', handleResize)
-        window.removeEventListener('mousemove', handleMouseMove)
       }
     }, [particleDensity]);
 
